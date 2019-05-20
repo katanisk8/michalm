1d93858060c2282704e93ab41ec12927bbf2df7e
Michal Makowej <michal@makowej.pl>
2019-05-20 17:55:04 +0200

    Change to asp.net core 3.0

---
+++ Kot/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Kot.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
